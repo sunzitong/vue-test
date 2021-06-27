0caf77168df284512c16f5ab411e39b56e04fa82
sunzitong <642626517@qq.com>
2021-06-27 16:21:03 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,31 @@
+  module.exports = {
+      publicPath: process.env.NODE_ENV === 'production' ? "/fe/customerinvitat/" : "",
+    //   css: {
+    //       loaderOptions: {
+    //           stylus: {
+    //               'resolve url': true,
+    //               'import': []
+    //           },
+    //           postcss: {
+    //               autoprefixer: {},
+    //               plugins: [
+    //                   require('postcss-px2rem')({
+    //                       remUnit: '37.5',
+    //                       propList: ['*']
+    //                   })
+    //               ]
+    //           }
+    //       }
+    //   },
+    //   devServer: {
+    //       proxy: {
+    //           '/coupon_api': {
+    //               target: 'http://m.test109.iguanyu.com/coupon_api',
+    //               changeOrigin: true,
+    //               pathRewrite: {
+    //                   '^/coupon_api': '/'
+    //               }
+    //           },
+    //       }
+    //   },
+  };
