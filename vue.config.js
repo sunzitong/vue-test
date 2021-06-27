a0d279dd95ca665f519c1e68ee72b761c8c58a7a
sunzitong <642626517@qq.com>
2021-06-27 17:45:52 +0800

    feat: chang publich path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
   module.exports = {
-      publicPath: process.env.NODE_ENV === 'production' ? "/fe/customerinvitat/" : "",
+      publicPath: process.env.NODE_ENV === 'production' ? "/" : "",
     //   css: {
     //       loaderOptions: {
     //           stylus: {
